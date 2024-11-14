09c107145d59e2ba8b3ec6b96777aff1d8ee4767
Lorenzo Hu <hu.lorenzo@iisgalvanimi.edu.it>
2024-11-14 08:24:43 +0000

    fase 3 completato

--- ListaSpesa/ListaSpesa.py
+++ ListaSpesa/ListaSpesa.py
@@ -1,12 +1,15 @@
 z=6
 spesa = []
+
 def aggiungiElementi():
     n = int(input("Quanti elementi di spesa vuoi inserire "))
     for i in range(n):
         elementi = input("Inserisci un elemento di spesa ")
         spesa.append(elementi)
+
 def stampaElementi():
         print(spesa)
+
 def eliminaElementi():
     stampaElementi()
     n = int(input("Quale elementi di spesa vuoi eliminare "))
@@ -14,6 +17,11 @@ def eliminaElementi():
         n = 0
     spesa.pop(n)
     stampaElementi()
+def contaElementi():
+        print(len(spesa))
+def svotaElementi():
+        spesa.clear()
+        print("Lista svotata")
 
 
 while z != 0 :
@@ -21,6 +29,8 @@ while z != 0 :
     print("N'1 comando per aggiungere elemento")
     print("N'2 comando per stampare elemento")
     print("N'3 comando per eliminare elemento")
+    print("N'4 comando per contare elemento")
+    print("N'5 comando per svotare elemento")
     z = int(input("Seleziona numero di comando: "))
     if z == 1:
         aggiungiElementi()
@@ -28,6 +38,10 @@ while z != 0 :
         stampaElementi()
     if z == 3:
         eliminaElementi()
+    if z == 4:
+        contaElementi()
+    if z == 5:
+        svotaElementi()
     if z == 0:
        break 
     
