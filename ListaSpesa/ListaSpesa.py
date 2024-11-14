9a1bb201793e68b5871376867a6ba81353921cbb
Lorenzo Hu <hu.lorenzo@iisgalvanimi.edu.it>
2024-11-14 07:32:42 +0000

    metodo aggiundi elemento fatto ,Punto 2

--- ListaSpesa/ListaSpesa.py
+++ ListaSpesa/ListaSpesa.py
@@ -1,3 +1,9 @@
 
 spesa = []
+def _aggiungiElementi():
+    n = int(input("Quanti elementi di spesa vuoi inserire"))
+    for i in range(n):
+        elementi = input("Inserisci un elemento di spesa")
+        spesa.append(elementi)
+_aggiungiElementi()
 
