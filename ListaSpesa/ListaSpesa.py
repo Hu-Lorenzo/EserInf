813b8ba60de32eedc77eeb3c6fe60841a2da0981
Lorenzo Hu <hu.lorenzo@iisgalvanimi.edu.it>
2024-11-14 07:35:12 +0000

    stampaElemento fatto ,punto 3

--- ListaSpesa/ListaSpesa.py
+++ ListaSpesa/ListaSpesa.py
@@ -1,9 +1,12 @@
 
 spesa = []
 def _aggiungiElementi():
-    n = int(input("Quanti elementi di spesa vuoi inserire"))
+    n = int(input("Quanti elementi di spesa vuoi inserire "))
     for i in range(n):
-        elementi = input("Inserisci un elemento di spesa")
+        elementi = input("Inserisci un elemento di spesa ")
         spesa.append(elementi)
+def _stampaElementi():
+        print(spesa)
 _aggiungiElementi()
+_stampaElementi()
 
