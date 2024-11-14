9d6f1ce690cd21ad743280124d815e672ce9785f
Lorenzo Hu <hu.lorenzo@iisgalvanimi.edu.it>
2024-11-14 08:00:22 +0000

    punto 5 a meta

--- ListaSpesa/ListaSpesa.py
+++ ListaSpesa/ListaSpesa.py
@@ -1,34 +1,30 @@
 z=6
 spesa = []
-def _aggiungiElementi():
+def aggiungiElementi():
     n = int(input("Quanti elementi di spesa vuoi inserire "))
     for i in range(n):
         elementi = input("Inserisci un elemento di spesa ")
         spesa.append(elementi)
-def _stampaElementi():
+def stampaElementi():
         print(spesa)
-def _eliminaElementi():
-    _stampaElementi()
+def eliminaElementi():
+    stampaElementi()
     n = int(input("Quale elementi di spesa vuoi eliminare "))
     if n == 1 :
         n = 0
     spesa.pop(n)
-    _stampaElementi()
-
-def _termina():
-    break
-
+    stampaElementi()
 
 
 while z != 0 :
-     if z == 1:
-        _aggiungiElementi()
-     if z == 2:
-        __stampaElementi()
-     if z == 3:
-        _eliminaElementi()
-     if z == 0:
-        _termina()
+    if z == 1:
+        aggiungiElementi()
+    if z == 2:
+        stampaElementi()
+    if z == 3:
+        eliminaElementi()
+    if z == 0:
+        
     
 
 
