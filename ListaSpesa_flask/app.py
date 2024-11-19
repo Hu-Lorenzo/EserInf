cd41cc9bd1d3aac45dc08213eeee00b587640f84
Lorenzo Hu <hu.lorenzo@iisgalvanimi.edu.it>
2024-11-19 08:44:58 +0000

    fase 3 punto 3 fatto a meta

--- ListaSpesa_flask/app.py
+++ ListaSpesa_flask/app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request,redirect, url_for
 #render_template: collegare file HTML.
 app = Flask(__name__)
 #rotta principale
+spesa = []
 @app.route('/')
 def home():
     return render_template('index.html')
+#Fatto il posto di aggiungi
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+    elemento = request.form['elemento']
+    if elemento:
+        spesa.append(elemento)
+        return redirect(url_for('home'))
 #avvio dell'app Flask
 if __name__ == '__main__':
     app.run(debug=True)
