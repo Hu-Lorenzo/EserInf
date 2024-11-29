7b7073e6b815bf9aedb1b5305ddf10fea501e1cf
Lorenzo Hu <hu.lorenzo@iisgalvanimi.edu.it>
2024-11-29 12:23:25 +0000

    sistemato il progetto 3 manca css

--- ListaSpesa_flask/app.py
+++ ListaSpesa_flask/app.py
@@ -1,32 +1,48 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
+from models import db, ListaSpesa  # Importa db e ListaSpesa dal file models
 
-# Crea l'app Flask
 app = Flask(__name__)
+# Configurazione del database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lista_spesa.db'  # Percorso del database SQLite
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disabilita il tracking delle modifiche
 
-# Lista della spesa inizialmente vuota
-spesa = []
+# Inizializza SQLAlchemy con l'app
+db.init_app(app)
 
-# Rotta principale per visualizzare la lista
+# Crea il database (se non esiste) all'avvio dell'app
+with app.app_context():
+    db.create_all()
+
+# Rotta principale per visualizzare la lista della spesa
 @app.route('/')
 def home():
-    return render_template('index.html', spesa=spesa)
+    lista_spesa = ListaSpesa.query.all()  # Ottieni tutti gli elementi
+    return render_template('index.html', lista=lista_spesa)
 
 # Rotta per aggiungere un elemento alla lista
 @app.route('/aggiungi', methods=['POST'])
 def aggiungi():
-    elemento = request.form['elemento']
-    if elemento:
-        spesa.append(elemento)
+    elemento = request.form.get('elemento') 
+    if elemento:  
+        nuovo_elemento = ListaSpesa(elemento=elemento)  # Crea un nuovo elemento
+        db.session.add(nuovo_elemento)  # Aggiungi il nuovo elemento alla sessione
+        db.session.commit()  # Salva i cambiamenti nel database
     return redirect(url_for('home'))
 
-# Rotta per rimuovere un elemento dalla lista, in base all'indice
-@app.route('/rimuovi/<int:indice>', methods=['GET'])
-def rimuovi(indice):
-    if 0 <= indice < len(spesa):
-        spesa.pop(indice)
+# Rotta per rimuovere un elemento dalla lista
+@app.route('/rimuovi/<int:id>', methods=['POST'])
+def rimuovi(id):
+    elemento = ListaSpesa.query.get_or_404(id)  # Trova l'elemento per ID, restituisce errore 404 se non trovato
+    db.session.delete(elemento)  # Elimina l'elemento
+    db.session.commit()  # Salva i cambiamenti nel database
     return redirect(url_for('home'))
 
-# Avvio dell'app Flask
+# Rotta per svuotare tutta la lista
+@app.route('/svuota', methods=['POST'])
+def svuota():
+    ListaSpesa.query.delete()  # Elimina tutti gli elementi nella tabella
+    db.session.commit()  # Salva i cambiamenti nel database
+    return redirect(url_for('home'))
+
 if __name__ == '__main__':
     app.run(debug=True)
-
--- ListaSpesa_flask/models.py
+++ ListaSpesa_flask/models.py
@@ -1,12 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-
 # Inizializziamo SQLAlchemy
 db = SQLAlchemy()
-
 # Definiamo la classe del modello ListaSpesa
 class ListaSpesa(db.Model):
     id = db.Column(db.Integer, primary_key=True)  # ID unico per ogni elemento
     elemento = db.Column(db.String(100), nullable=False)  # Descrizione dell'elemento
 
-    def __repr__(self):
-        return f"<Elemento {self.elemento}>"
